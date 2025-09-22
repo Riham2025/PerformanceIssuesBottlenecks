02ddb5ca9ede479477dbf10f14568b27c3a245fb
riham <riham91.alsiyabi@gmail.com>
2025-09-22 09:31:25 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PerformanceIssuesBottlenecks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
